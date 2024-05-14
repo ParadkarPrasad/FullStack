aa31c6f51d04df3f247b2f1fb901a8250fb6084e
ParadkarPrasad <prasadparad37@gmail.com>
2024-05-14 14:41:09 +0100

    Exercise 4.18- 4.19

--- Part04/controllers/blogs.js
+++ Part04/controllers/blogs.js
@@ -1,18 +1,29 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+const getTokenFrom = request =>{
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/',async (req,res) => {
  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1,})
     res.json(blogs);
   })
 
+blogRouter.post('/', async (request,response ,next)=> {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id){
+    return response.status(401).json({error: 'invalid token or missing'})
+  }
 
-blogRouter.post('/', async (req,res,next)=> {
-  const body = req.body
-  const user = await User.findById(body.userId)
-
+  const user = await User.findById(decodedToken.id)
   if(!body.title || !body.url){
     return res.status(400).end()
   }
@@ -21,12 +32,12 @@ blogRouter.post('/', async (req,res,next)=> {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user.id
+    user: user._id
   })
  const savedBlog = await blog.save()
  user.blogs = user.blogs.concat(savedBlog._id)
  await user.save()
- res.status(201).json(savedBlog)
+ response.status(201).json(savedBlog)
   })
   
   blogRouter.delete('/:id', async(req,res)=>{
